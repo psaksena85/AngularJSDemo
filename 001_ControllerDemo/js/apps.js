93e056a1cbab493cdc35cee081d5fc408244a3af
Pulkit Saxena <pulkit_saxena@apple.com>
2017-05-29 20:09:35 -0700

    Controller Demo

--- 001_ControllerDemo/js/apps.js
+++ 001_ControllerDemo/js/apps.js
@@ -1,1 +1,7 @@
 var app = angular.module("controllerDemo", []);
+
+app.controller("ControllerDemoCtrl", ['$scope', function($scope) {
+    $scope.user = {};
+    $scope.user.name = "Pulkit Saxena";
+    $scope.user.address = "San Jose, California";
+}]);
